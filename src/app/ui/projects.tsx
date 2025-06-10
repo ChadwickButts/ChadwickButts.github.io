0e637a6992d7da5f0c67301ebb5344c0334cf6ac
Chadwick Butts <ccbutts@mail.usf.edu>
2025-06-10 11:38:41 -0400

    added some links

--- src/app/ui/projects.tsx
+++ src/app/ui/projects.tsx
@@ -17,13 +17,26 @@ export default function Projects() {
                                 <a className="font-medium"
                                     href={project.link}
                                     target="_blank" rel="noopener noreferrer" >
-                                    <img src={project.img} width={400} height={200}/>
+                                    <img src={project.img} width={400} height={200} />
                                     {project.name}
                                 </a>
                             </div>
                         </div>
                     })
                 }
+            </div>
+            <br />
+            <div >
+                <h3 className="text-2xl">Interesting Links: </h3>
+                <ul>
+                    <li>
+                        <a className="hover:underline" target="_blank" href="https://github.com/ChadwickButts" >GitHub</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" target="_blank" href="https://codepen.io/ccbutts529" >Codepen</a>
+                    </li>
+                </ul>
+                
             </div>
         </div>
     )
